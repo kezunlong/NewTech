6fc6f0c1c1bb2626577c572f22cdb54f947d5ded
kezunlong <13761596352@163.com>
2015-09-06 08:07:14 +0800

    add bootstrap

---
+++ NewTech.Web/Models/AlertMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewTech.Web.Models
+{
+    public class AlertMessage
+    {
+        public AlertCategory Category { get; set; }
+        public string Message { get; set; }
+        public string CategoryCSS
+        {
+            get
+            {
+                switch (Category)
+                {
+                    case AlertCategory.Success: return "alert alert-success";
+                    case AlertCategory.Info: return "alert alert-info";
+                    case AlertCategory.Warning: return "alert alert-warning";
+                    case AlertCategory.Error: return "alert alert-danger";
+                    default: return "alert alert-info";
+                }
+
+            }
+        }
+
+        public AlertMessage(string message)
+            : this(AlertCategory.Success, message)
+        {
+        }
+
+        public AlertMessage(AlertCategory category, string message)
+        {
+            this.Category = category;
+            this.Message = message;
+        }
+    }
+
+    public enum AlertCategory
+    {
+        Success,
+        Info,
+        Warning,
+        Error
+    }
+}
