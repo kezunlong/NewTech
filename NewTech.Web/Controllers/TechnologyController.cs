c8db4f61c15c1aa3f5f77bb445ce1e0859f5fb31
kezunlong <13761596352@163.com>
2015-10-09 17:59:15 +0800

    mini change

---
+++ NewTech.Web/Controllers/TechnologyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewTech.Web.Controllers
+{
+    public class TechnologyController : BaseController
+    {
+        // GET: Technology
+        public ActionResult MicrosoftDotNet()
+        {
+            ViewBag.ImportantProjects = bll.ProjectManager.SelectImportantProjects("1001", 3);
+
+            return View();
+        }
+    }
+}
