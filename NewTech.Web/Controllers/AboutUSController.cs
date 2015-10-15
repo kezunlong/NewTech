6b0a34515410b4c2d91d9dd0f0d0a6af93f55c51
kezunlong <13761596352@163.com>
2015-10-15 16:03:03 +0800

    About US

---
+++ NewTech.Web/Controllers/AboutUSController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewTech.Web.Controllers
+{
+    public class AboutUSController : Controller
+    {
+        // GET: AboutUS
+        public ActionResult Company()
+        {
+            return View();
+        }
+
+        public ActionResult WhyNewTech()
+        {
+            return View();
+        }
+
+        public ActionResult VisionAndValue()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUS()
+        {
+            return View();
+        }
+    }
+}
